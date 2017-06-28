e30c8b0ad95fb6657b0d92e2487da218504593fd
Michael Wornow <miking98@fb.com>
2017-06-28 11:05:07 -0700

    Link all elements of Post table cell to feedTableViewCell class

--- finsta/feedTableViewCell.swift
+++ finsta/feedTableViewCell.swift
@@ -11,11 +11,24 @@ import Parse
 import ParseUI
 
 class feedTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var userProfileImageView: UIImageView!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var createdDateLabel: UILabel!
     @IBOutlet weak var imagePFView: PFImageView!
+    
+    
+    @IBOutlet weak var heartButtonImageView: UIImageView!
+    @IBOutlet weak var commentButtonImageView: UIImageView!
+    @IBOutlet weak var sendButtonImageView: UIImageView!
+    @IBOutlet weak var bookmarkButtonImageView: UIImageView!
+    
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var topCommentUsernameLabel: UILabel!
+    @IBOutlet weak var topCommentContentLabel: UILabel!
+    @IBOutlet weak var viewCommentsLabel: UILabel!
+    
+    @IBOutlet weak var createdDateLabel: UILabel!
     
     var postImageFile: PFFile! {
         didSet {
